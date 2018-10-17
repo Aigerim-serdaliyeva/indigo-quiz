7300262edf89a23bc91c35f8cece4ad6074d4cc2
Aigerim-serdaliyeva <33958074+Aigerim-serdaliyeva@users.noreply.github.com>
2018-10-17 16:16:15 +0600

    fixes

--- app/mail.php
+++ app/mail.php
@@ -4,10 +4,10 @@ if(isset($_POST["info"])) {
 
   $method = $_SERVER['REQUEST_METHOD'];
 
-  $project_name = "";
-  $admin_email  = "info@.kz, client@marketing-time.kz";
-  $server_mail = "<info@.kz>";
-  $form_subject = "Заявка";
+  $project_name = "Avtomatizacia.kz/amo";
+  $admin_email  = "mtimeemail@yandex.kz, client@marketing-time.kz";
+  $server_mail = "<admin@yandex.kz>";
+  $form_subject = "Quiz";
 
 
   //Script Foreach
@@ -40,8 +40,8 @@ if(isset($_POST["info"])) {
   'From: '.$project_name.' '.$server_mail. PHP_EOL .
   'Reply-To: '.$admin_email.'' . PHP_EOL;
 
-  mail($admin_email, $form_subject, $message, $headers);
+  mail($admin_email, adopt($form_subject), $message, $headers);
 
-  header("Location: /thanks.html");
+  header("Location: /amo/quiz/thanks.html");
   
 } 
